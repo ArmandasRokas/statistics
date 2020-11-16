6f766607745cd92b272f3819af3eb4695ea6e05b
Armandas Rokas <armandas.rokas@gmail.com>
2020-11-16 10:44:16 +0100

    added some notes to exam

--- exam/method_3.51_two_sample_t.test_parameter_mean_sd.R
+++ exam/method_3.51_two_sample_t.test_parameter_mean_sd.R
@@ -21,10 +21,10 @@ t.test2 <- function(m1,m2,s1,s2,n1,n2,m0=0,equal.variance=FALSE, conf.int=0.05)
   }      
   CI <- m1-m2 + c(-1,1)*qt(1-conf.int/2, df)*sqrt(s1^2/n1+s2^2/n2)
   t <- (m1-m2-m0)/se 
-  dat <- c(m1-m2,df , se, t, 2*pt(-abs(t),df), CI)    
+  dat <- c(m1-m2,df , se, t, 2*(1-pt(abs(t),df)), CI)    
   names(dat) <- c("Difference of means", "df" , "Std Error", "t", "p-value", "CI low", "CI high")
   
   
   return(dat) 
 }
-t.test2(m1=1.93, m2=1.49, s1=0.45, s2=0.58, n1=9, n2=9)
+t.test2(m1=78.34444, m2=74.29000, s1=1.898, s2=2.115 , n1=9, n2=10, conf.int = 0.01)
